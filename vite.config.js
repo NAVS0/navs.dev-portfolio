a5916968109f4946b94f67d164ceab5752ee57eb
Navs.Devs <rogelnavarro74@gmail.com>
2025-08-25 17:04:40 +0800

    committ

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/navs-dev-portfolio/",
+  base: "/navs.dev-portfolio/",
   server: {
     open: true,
     port: 300, // 👈 fixed typo
