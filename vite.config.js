557ca0b168c04853a6e4b3aa2407debc1217c1fd
Navs.Devs <rogelnavarro74@gmail.com>
2025-08-25 15:28:13 +0800

    added workflow dispatch

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/navs-devs-portfolio/",
+  base: "/navs-dev-portfolio/",
   server: {
     open: true,
     por: 300,
