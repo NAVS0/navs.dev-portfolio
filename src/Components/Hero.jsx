bd547ab329d513be93f41a9fb9b9b6daf9119405
Navs.Devs <rogelnavarro74@gmail.com>
2025-08-25 15:37:41 +0800

    commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div id='home' className = 'hero'>
         <img src={profile} alt="" />
-        <h1><span>I'm Rogel Navarro,</span> frontend developer based in Philippines</h1>
+        <h1><span>I'm Rogel T. Navarro,</span> frontend developer based in Philippines</h1>
         <p>I’m a frontend developer and designer passionate about creating sleek, <br /> user-friendly websites, smart apps, and innovative digital experiences.</p>
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With me</AnchorLink></div>
