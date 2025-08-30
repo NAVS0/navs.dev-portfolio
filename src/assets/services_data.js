5eea6748ca8c2f257bb1515429d41e21c6004dae
Navs.Devs <rogelnavarro74@gmail.com>
2025-08-30 15:26:33 +0800

    AUG. 30, 2025 commit

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -2,22 +2,22 @@ const Services_Data = [
     {
         s_no:"01",
         s_name:"Web Development",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Web development is the process of building and maintaining websites and web applications..."
     },
     {
         s_no:"02",
         s_name:"Game Development",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Game development is the process of designing, coding, and creating interactive digital..."
     },
     {
         s_no:"03",
-        s_name:"UX/UI Design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"UI Design",
+        s_desc:"UI (User Interface) is the design of a product’s look and feel — the buttons, layouts..."
     },
     {
         s_no:"04",
         s_name:"Graphic Design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Graphic design is the art of creating visual content to communicate ideas and messages effectively."
     },
 ]
  
